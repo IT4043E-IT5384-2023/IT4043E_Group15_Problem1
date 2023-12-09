3069d614595ef52ee953151dd2bcf516d3c0a7bc
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-12-09 21:40:40 +0700

    Fix indents

--- twitter/data_processing/encode_tweets.py
+++ twitter/data_processing/encode_tweets.py
@@ -31,25 +31,25 @@ def encode_tweets():
     for key in keys:
         if key not in ['rawDescription', 'tweet']:
             processed_data[key] = user_data[key]
-        processed_data['descriptionVec'] = {}
-        for i in range(no_user):
-            processed_data['descriptionVec'][str(i)] = encode_tweet(str(user_data['rawDescription'][str(i)]), model, tokenizer)
-        processed_data['tweet'] = {}
-        for i in range(no_user):
-            processed_data['tweet'][str(i)] = []
-            for tweet in user_data['tweet'][str(i)]:
-                new_tweet = {}
-                for tweet_key in ['date','replyCount','retweetCount','likeCount' ,'quoteCount', 'viewCount']:
-                    new_tweet[tweet_key] = tweet[tweet_key]
-                if tweet['place'] is None:
-                    new_tweet['country'] = None
-                    new_tweet['countryCode'] = None
-                else:
-                    new_tweet['country'] = tweet['place']['country']
-                    new_tweet['countryCode'] = tweet['place']['countryCode']
-                new_tweet['lang'] = LANG_DICT[tweet['lang']]
-                new_tweet['textVec'] = encode_tweet(str(tweet['rawContent']), model, tokenizer)
-                processed_data['tweet'][str(i)].append(new_tweet)
+    processed_data['descriptionVec'] = {}
+    for i in range(no_user):
+        processed_data['descriptionVec'][str(i)] = encode_tweet(str(user_data['rawDescription'][str(i)]), model, tokenizer)
+    processed_data['tweet'] = {}
+    for i in range(no_user):
+        processed_data['tweet'][str(i)] = []
+        for tweet in user_data['tweet'][str(i)]:
+            new_tweet = {}
+            for tweet_key in ['date','replyCount','retweetCount','likeCount' ,'quoteCount', 'viewCount']:
+                new_tweet[tweet_key] = tweet[tweet_key]
+            if tweet['place'] is None:
+                new_tweet['country'] = None
+                new_tweet['countryCode'] = None
+            else:
+                new_tweet['country'] = tweet['place']['country']
+                new_tweet['countryCode'] = tweet['place']['countryCode']
+            new_tweet['lang'] = LANG_DICT[tweet['lang']]
+            new_tweet['textVec'] = encode_tweet(str(tweet['rawContent']), model, tokenizer)
+            processed_data['tweet'][str(i)].append(new_tweet)
 
     with open(PROCESSED_ALL_PATH, 'w') as f:
         json.dump(processed_data, f, indent=4)
